221736ead82a32b2c7d3ecbd46b5c192b91c0735
yangtao4783 <2649388386@qq.com>
2017-05-14 19:31:36 +0800

    商品详情

--- src/modules/goodsDetail/routes/index.js
+++ src/modules/goodsDetail/routes/index.js
@@ -9,7 +9,7 @@ import consultList from './consultList';
 import consultEdit from './consultEdit';
 
 const routesConfig = (<Route path="/" component={App}>
-  <Route path='/:specId' component={goodsDetail} title='商品详情' />
+  <Route path='/:goodsId' component={goodsDetail} title='商品详情' />
   <Route path='multiSpec/:specId' component={mutiSpecGoodsDetail} title='商品详情'/>
   <Route path='evaluteList/:goodsId' component={evaluteList} title='评价' />
   <Route path='consultList/:goodsId' component={consultList} title='购买咨询'/>
