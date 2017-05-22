c15492c7ce0de5cb92d9e046a97caa1ddc8e3684
shibaomi <shibm890130@163.com>
2017-05-23 00:24:33 +0800

    订单

--- src/modules/order/routes/order.js
+++ src/modules/order/routes/order.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 import {
-  Modal,
-  WhiteSpace,
-  WingBlank,
-  Toast,
-  Flex,
-  Button,
-  List,
-  Switch,
-  Popup
+    Modal,
+    WhiteSpace,
+    WingBlank,
+    Toast,
+    Flex,
+    Button,
+    List,
+    Switch,
+    Popup
 } from 'antd-mobile';
 import { Img } from 'commonComponent';
 import * as orderApi from '../api/order';
@@ -18,6 +18,7 @@ import { common } from 'common';
 import Shop from '../components/Shop';
 import Fee from '../components/Fee';
 import OrderBar from '../components/OrderBar';
+import * as addressApi from '../api/address';
 
 const prompt = Modal.prompt;
 const Item = List.Item;
@@ -28,19 +29,23 @@ import './order.less';
 class Order extends Component {
   constructor(props) {
     super(props);
+    this.state={
+      selectCarts:[],
+      selectedAddress:''
+    };
   }
 
   submitOrder = () => {
     // 提交订单
     const {
-      selectedAddress,
-      paytype,
-      isPd,
-      freight,
-      couponId,
-      invoice,
-      priceData,
-      cartId
+        selectedAddress,
+        paytype,
+        isPd,
+        freight,
+        couponId,
+        invoice,
+        priceData,
+        cartId
     } = this.props.order;
     orderApi.saveorder({
       cartIds: cartId,
@@ -80,13 +85,13 @@ class Order extends Component {
 
     // 提交订单
     const {
-      selectedAddress,
-      paytype,
-      isPd,
-      freight,
-      couponId,
-      invoiceId,
-      priceData,
+        selectedAddress,
+        paytype,
+        isPd,
+        freight,
+        couponId,
+        invoiceId,
+        priceData,
     } = this.props.order;
     // 验证数据
     if (!selectedAddress) {
@@ -97,21 +102,21 @@ class Order extends Component {
     // 如果使用余额支付，弹出密码输入框，，否则跳到
     if (isPd == 1 && paytype == 1) {
       prompt(
-        '请输入支付密码',
-        '', [{ text: '取消' }, {
-          text: '提交',
-          onPress: passwd => {
-            orderApi.chkPasswd({ passwd }).then(result => {
-              if (result.result == 1) {
-                // 密码正确，继续提交订单
-                this.submitOrder();
-              } else {
-                Toast.fail(result.msg);
-              }
-            })
-          }
-        }],
-        'secure-text',
+          '请输入支付密码',
+          '', [{ text: '取消' }, {
+            text: '提交',
+            onPress: passwd => {
+              orderApi.chkPasswd({ passwd }).then(result => {
+                if (result.result == 1) {
+                  // 密码正确，继续提交订单
+                  this.submitOrder();
+                } else {
+                  Toast.fail(result.msg);
+                }
+              })
+            }
+          }],
+          'secure-text',
       )
 
     } else {
@@ -143,243 +148,139 @@ class Order extends Component {
     this.props.router.push('/address');
   }
 
-  onClickCoupon = () => {
-    if (this.props.order.couponCount == 0) {
-      return;
-    }
-    this.props.router.push(`/coupon/${this.props.params.cartId}`);
-  }
 
   onClickInvoice = (invoiceShow) => {
-  	console.log(invoiceShow)
-  	let invContent=1
-  	if(invoiceShow!='不开发票'){
-  			invContent=2
-  	}else{
-  			invContent=1
-  	}
+    console.log(invoiceShow)
+    let invContent=1
+    if(invoiceShow!='不开发票'){
+      invContent=2
+    }else{
+      invContent=1
+    }
 //  this.props.router.push('/invoice/${invoiceShow,invContent}');
     this.props.router.push(`/invoice/${invoiceShow}/${invContent}`);
   }
 
-  onChangePd = (checked) => {
-    // 刷新价格显示
-    const { paytype, couponId, selectedAddress, cartId, cartVoList } = this.props.order;
-    const isPd = checked ? 1 : 0;
-    const freight = cartVoList.map(cart => {
-      return cart.selectedShip + "|" + cart.storeId;
-    })
-    orderApi.getPrice({
-      cartIds: cartId,
-      cityId: selectedAddress.cityId,
-      freight: freight.join(),
-      couponId,
-      isPd
-    }).then(r => {
-      const priceData = r.data[0];
-      this.props.dispatch({
-        type: 'changePd',
-        payload: {
-          priceData,
-          isPd
-        }
-      })
-    })
-  }
-
-  updateShip = ({ storeId, shipType }) => {
-    this.props.dispatch({
-      type: 'updateShip',
-      payload: {
-        storeId,
-        shipType
-      }
-    })
-
-    const {
-      isPd,
-      cartId,
-      selectedAddress,
-      couponId,
-      cartVoList
-    } = this.props.order;
-
-    const freight = cartVoList.map(cart => {
-      return cart.selectedShip + "|" + cart.storeId;
-    })
-
-    orderApi.getPrice({
-      cartIds: cartId,
-      cityId: selectedAddress.cityId,
-      isPd,
-      freight: freight.join(),
-      couponId
-    }).then(r => {
-      const priceData = r.data[0];
-      this.props.dispatch({
-        type: 'getPrice',
-        payload: priceData
-      })
-    })
-
-  }
-
   componentDidMount() {
-    const { isPd, freight, paytype, couponId, isInit } = this.props.order;
-    if (!isInit) {
-      return;
-    }
-    const cartId = this.props.params.cartId;
-    console.log(cartId);
-    orderApi.subToOrder({ cartId }).then(result => {
+    this.setState({
+      selectCarts:JSON.parse(localStorage.getItem("selectCarts"))
+    });
+    this.initAddressList();
+  }
+
+  initAddressList = () => {
+    addressApi.addressList().then(result => {
       if (result.result == 1) {
-        const data = result.data[0];
-        // console.log(data);
-
-        if (data.addressList && data.addressList.length > 0) {
-          let currentSelectedAddress = data.addressList[0];
-          orderApi.addShipping({
-            cartIds: cartId,
-            cityId: currentSelectedAddress.cityId
-          }).then(r => {
-            if (result.result == 1) {
-              // this.props.dispatch({
-              //   type: 'addShipping',
-              //   payload: r.data[0]
-              // })
-              this.props.dispatch({
-                type: 'init',
-                payload: data,
-                cartId,
-                shipData: r.data[0]
-              })
-
-              const freight = [];
-              if (r.data && r.data[0]) {
-                const keys = Object.keys(r.data[0])
-                keys.map(storeId => {
-                  const shipObj = r.data[0][storeId]
-                  const freightItem = Object.keys(shipObj)[0] + "|" + storeId
-                  freight.push(freightItem)
-                })
-              }
-
-              // TDOO: 获取默认物流信息,是否使用余额
-              // console.log(result.data[0]);
-              // console.log(r.data[0]);
-
-              orderApi.getPrice({
-                cartIds: cartId,
-                cityId: currentSelectedAddress.cityId,
-                isPd,
-                freight: freight.join(),
-                couponId
-              }).then(r => {
-                const priceData = r.data[0];
-                this.props.dispatch({
-                  type: 'getPrice',
-                  payload: priceData
-                })
-              })
-
-            } else {
-              Toast.fail(result.msg);
-            }
-          })
-        }
+        result.data&&result.data.map((addr,index) => {
+          if(addr.isDefault==1){
+            this.setState({
+              selectedAddress : addr
+            });
+          }
+        })
       }
     })
   }
 
   render() {
-    const {
-      cartVoList,
-      selectedAddress,
-      couponCount,
-      memberAvailable,
-      priceData,
-      shipData,
-      isPd,
-      paytype,
-      invoice
-    } = this.props.order;
-    let couponShow = couponCount > 0 ? `${couponCount}张优惠券` : '无可用优惠券';
-    if (priceData.couponPrice != '0.0') {
-      couponShow = `¥${priceData.couponPrice}`
-    }
-    let invoiceShow = '不开发票'
-    // 已填写发票，并且选择的是明细
-    if (invoice && invoice.invId && invoice.invContent == 2) {
-      invoiceShow = invoice.invTitle
-    }
     return <div className='wx-order'>
       <div className='fix-scroll hastitle' style={{paddingBottom:'1.1rem'}}>
-      <List>
-        <Item onClick={this.onClickSelectedAddress}
-          arrow="horizontal"
-          multipleLine>
-          {
-            selectedAddress ?<div>
-              {selectedAddress.mobPhone}&nbsp;&nbsp; {selectedAddress.trueName}
-              <Brief>{selectedAddress.areaInfo} {selectedAddress.address}</Brief>
-            </div>: '请选择地址'
-          }
-        </Item>
-      </List>
-      {
-        cartVoList.map((shop, index) => {
-            return <Shop updateShip={this.updateShip} key={index} data={shop}></Shop>
-        })
-      }
-      <List>
-        <Item
-          onClick={this.onSelectPayTypeClick}  
-          arrow="horizontal"
-          extra={paytype==1?'在线支付':'货到付款'}
+        <List >
+          <Item onClick={this.onClickSelectedAddress} multipleLine arrow="horizontal"
+                style = {{padding:'0.2rem 0.3rem 0rem 0.3rem', border:'none'}}>
+            {
+            this.state.selectedAddress&&
+            <Flex>
+              <div>
+                <div style={{ backgroundImage: 'url(../../../assets/img/dingwei.png)', backgroundSize: 'cover', height: '0.4rem', width: '0.35rem' }}/>
+              </div>
+              <Flex.Item>
+                <div style = {{position:'relative'}}>
+                  <div style = {{width:'4.3rem', position:'relative',height:'0.5rem'}}>
+                    <div style = {{width:'1rem', position:'absolute',left:'0rem',}}>报告接收人</div>
+                    <div style = {{width:'2rem', position:'absolute',left:'1.5rem',}}>{this.state.selectedAddress.trueName}</div>
+                    <div style = {{width:'1rem', position:'absolute',left:'2.5rem',}}>
+                      {common.phoneDesensitization(this.state.selectedAddress.mobPhone)}
+                    </div>
+                  </div>
+                  <div style = {{width:'4.3rem', position:'relative',height:'0.5rem'}}>
+                    <div style = {{width:'1rem', position:'absolute',left:'0rem',}}>{this.state.selectedAddress.areaInfo} {this.state.selectedAddress.address}</div>
+                  </div>
+                </div>
+              </Flex.Item>
+            </Flex>
+            }
+            {
+              this.state.selectedAddress.length == 0 &&
+              <div style={{ padding:'20px 20px' }}>
+                <span style={{fontSize: '28px',color:'gray'}}>还没添加地址，去添加地址</span>
+              </div>
+            }
+          </Item>
+        </List>
+        {
+          this.state.selectCarts&&this.state.selectCarts.map((goods,index) => {
+            return <Item key={index}>
+              <Flex style = {{paddingLeft:'0.2rem'}}>
+                <Img src={common.imgtest + goods.goodsImages} style={{ height: '1.5rem', width: '1.5rem', marginLeft:'0.1rem' }} />
+                <Flex.Item>
+                  <div className='text-overflow-hidden' >{goods.goodsName}</div>
+                  <Flex justify='between'>
+                    <div style={{width: '45%',paddingBottom:'0.1rem'}}>{goods.packageName}</div>
+                    <div >{`x${goods.goodsNum}`}</div>
+                    <div style={{ color:'red',paddingTop:'0.1rem'}}>{`￥${goods.goodsPrice}`}</div>
+                  </Flex>
+                  <Flex justify='between'>
+                    <div >纸质报告正副本</div>
+                    <div >{`x${goods.report}`}</div>
+                    <div style={{ color:'red'}}>{`￥${goods.reportPrice}`}</div>
+                  </Flex>
+                </Flex.Item>
+              </Flex>
+            </Item>
+          })
+        }
+        }
+        <List>
+          <Item
+              onClick={this.onSelectPayTypeClick}
+              arrow="horizontal"
+              extra={1==1?'在线支付':'货到付款'}
           >
-          支付方式
-        </Item>
-        <Item
-          onClick={this.onClickCoupon}  
-          arrow="horizontal"
-          extra={couponShow}
+            支付方式
+          </Item>
+          <Item
+              extra={<div><Switch checked={1 == 1} /></div>}
+          >余额支付</Item>
+          <Item
+
+          >&nbsp;</Item>
+          <Item
+              arrow="horizontal"
           >
-          优惠券
-        </Item>
-        <Item
-          extra={<div><Switch checked={isPd == 1} onChange={this.onChangePd} /></div>}
-        >余额支付</Item>
-        <Item
-          extra={memberAvailable}
-        >&nbsp;</Item>
-        <Item
-          onClick={()=>this.onClickInvoice(invoiceShow)}    
-          arrow="horizontal"
-          extra={invoiceShow}
-          >
-          发票信息
-        </Item>
-      </List>
-      <WhiteSpace></WhiteSpace>
-      <Flex>
-        <Flex.Item style={{flex:2.5,borderRight: '1px solid #ddd'}}>
-          <List className="extrared">
-            <Item extra={`¥${priceData.totalGoodsPrice}`}>商品总价</Item>
-            <Item extra={`+ ¥${priceData.totalFreight}`}>运费</Item>
-            <Item extra={`- ¥${priceData.predepositAmount}`}>余额支付</Item>
-            <Item extra={`- ¥${priceData.couponPrice}`}>抵用券</Item>
-            <Item extra={`- ¥${priceData.conditionPrice}`}>优惠促销</Item>
-          </List>
-        </Flex.Item>
-        <Flex.Item style={{ flex: 1,background:'#fff'}}>
-          <div>
-            <div style={{height:'1.5rem',textAlign:'center',lineHeight:'2.5rem'}}>共需支付</div>
-            <div style={{color:'red',height:'1.5rem',textAlign:'center',lineHeight:'0.5rem'}}>{`¥${priceData.totalPrice}`}</div>
-          </div>
-        </Flex.Item>
+            发票信息
+          </Item>
+        </List>
+        <WhiteSpace></WhiteSpace>
+        <Flex>
+          <Flex.Item style={{flex:2.5,borderRight: '1px solid #ddd'}}>
+            <List className="extrared">
+              <Item >商品总价</Item>
+              <Item >运费</Item>
+              <Item >余额支付</Item>
+              <Item >抵用券</Item>
+              <Item>优惠促销</Item>
+            </List>
+          </Flex.Item>
+          <Flex.Item style={{ flex: 1,background:'#fff'}}>
+            <div>
+              <div style={{height:'1.5rem',textAlign:'center',lineHeight:'2.5rem'}}>共需支付</div>
+              <div style={{color:'red',height:'1.5rem',textAlign:'center',lineHeight:'0.5rem'}}></div>
+            </div>
+          </Flex.Item>
         </Flex>
-      </div>  
-      <OrderBar onSubmitOrder={this.onSubmitOrder} totalPrice={priceData.totalPrice}></OrderBar>
+      </div>
+      <OrderBar ></OrderBar>
     </div>
   }
 }
